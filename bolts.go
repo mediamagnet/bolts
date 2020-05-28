8c7aca97c9ebd42ce6d0d188ee6d4c8c7649578c
MediaMagnet <nathanec@gmail.com>
2020-05-28 13:04:31 -0500

    changed logrus level to error.

--- bolts.go
+++ bolts.go
@@ -16,7 +16,7 @@ var log = &logrus.Logger{
 	Out:       os.Stderr,
 	Formatter: new(logrus.TextFormatter),
 	Hooks:     make(logrus.LevelHooks),
-	Level:     logrus.DebugLevel,
+	Level:     logrus.ErrorLevel,
 }
 
 func main() {
